721e8b48db5308043615fe27c6118b926cc74fb5
zmh <1095058962@qq.com>
2018-06-03 12:14:09 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'styles/border.css'
+import 'styles/reset.css'
+import 'styles/iconfont.css'
+import 'babel-polyfill'
+import store from './store'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import fastclick from 'fastclick'
+import 'swiper/dist/css/swiper.css'
+
+Vue.config.productionTip = false
+fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/经典/游玩主题
+    </div>
+    <router-link to="/city">
+    <div class="header-right">
+      {{this.city}}
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+  name: 'HomeHeader',
+  computed: {
+    ...mapState(['city'])
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display: flex;
+    line-height: .86rem;
+    background: $bgColor;
+    color: #fff;
+    .header-left
+      width: .64rem;
+      float: left;
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      line-height: .64rem;
+      flex: 1;
+      background: #fff;
+      border-radius: .1rem;
+      height: .64rem;
+      margin-top: .12rem;
+      margin-left: .2rem;
+      color: #ccc;
+    .header-right
+      color: #fff;
+      min-width: 1.04rem;
+      padding: 0 .1rem
+      float: right;
+      text-align: center;
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+
+</style>
